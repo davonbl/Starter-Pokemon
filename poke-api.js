cbefeb45dcd60ca691a7bd6275d0cdd8348456d6
davonbl-bl <davonbl@gmail.com>
2023-12-05 21:10:55 -0500

    added some styling content ex: hovering over an element shakes

--- poke-api.js
+++ poke-api.js
@@ -3,22 +3,27 @@ import axios from "axios";
 
 const getPokemon = async() => {
     let pokemonNames = [];
-    for(let i = 1; i <= 10; i++ ){
+    for(let i = 1; i <= 6; i++ ){
         const randomPokemon = Math.floor(Math.random() * 350)
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`)
         let pokemonName = await response.data.name;
         let pokemonImages = await response.data.sprites;
 
         let showPokeImg = document.createElement('img');
-        showPokeImg.src = pokemonImages.front_default
+        showPokeImg.src = pokemonImages.front_default;
 
-        let showPokeName = document.createElement('p')
-        showPokeName.innerText = pokemonName
+        let showPokeName = document.createElement('p');
+        showPokeName.innerText = pokemonName;
 
-        document.body.append(showPokeImg, showPokeName)
+        let childContainer = document.createElement('div'); 
+
+        childContainer.append(showPokeImg, showPokeName);
+
+        let parentContainer = document.querySelector('#app')
+        parentContainer.append(childContainer)
 
         // console.log('testing: ', pokemonImages.front_default)
-        pokemonNames.push({pokemonName, pokemonImages})
+        pokemonNames.push({pokemonName, pokemonImages});
     }
     return pokemonNames
 }
